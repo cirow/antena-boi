9ff3326b38cab7ae25544c6026fb3857720dc3dc
Unknown <juliana.rubenich@gmail.com>
2018-03-19 22:37:29 -0300

    Tela inicial e de creditos com seleção funcionando por teclado e mouse

--- babySpace/Assets/Scripts/Managers/CreditsManager.cs
+++ babySpace/Assets/Scripts/Managers/CreditsManager.cs
@@ -7,9 +7,9 @@ public class CreditsManager : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
 	{
-		AudioManager.instance.WindAudio(false);
-		AudioManager.instance.InterfAudio(false);
-		AudioManager.instance.VictoryAudio(true);
+		//AudioManager.instance.WindAudio(false);
+		//AudioManager.instance.InterfAudio(false);
+		//AudioManager.instance.VictoryAudio(true);
 	}
 
 	// Update is called once per frame
